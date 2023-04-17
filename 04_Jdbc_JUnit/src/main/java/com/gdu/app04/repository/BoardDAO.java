e0ca5a4623441d55f28f6bc96f54145b24b421aa
yr1486 <yr1486@gmail.com>
2023-04-17 15:57:54 +0900

    2023-04-17

--- 04_Jdbc_JUnit/src/main/java/com/gdu/app04/repository/BoardDAO.java
+++ 04_Jdbc_JUnit/src/main/java/com/gdu/app04/repository/BoardDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
@@ -35,7 +36,8 @@ public class BoardDAO {
 	// private 메소드 - 2 (BoardDAO 클래스 내부에서만 사용)
 	private void close() {
 		try {
-			if(rs != null) rs.close();
+			if(rs != null) rs.
+			close();
 			if(ps != null) ps.close();
 			if(con != null) con.close();
 		}catch (Exception e) {
@@ -50,19 +52,120 @@ public class BoardDAO {
 	
 	// 1. 목록
 	public List<BoardDTO> selectBoardList() {
+		List<BoardDTO> list = new ArrayList<BoardDTO>();
+		list.add(new BoardDTO(1, "제목", "내용", "작성자", "작성일", "수정일"));
 		
-	}
+		try {
+			con = getConnection(); // 시작은 커넥션 모든 DAO 메소드에서 변치 않음
+			sql = "SELECT BOARD_NO, TITLE, CONTENT, WRITER, CREATED_AT, MODIFIED_AT FROM BOARD ORDER BY BOARD_NO DESC";
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			
+			while(rs.next()) { 
+				BoardDTO board = new BoardDTO(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6));
+				list.add(board); // array리스트에 담기
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(); // 자원반납
+		}
+		return list;
+	} 
+	
+	
 	
 	// 2. 상세
 	
+	public BoardDTO selectBoardByNo(int board_no) {
+		BoardDTO board = null;
+		try {
+			con = getConnection(); // 시작은 커넥션 모든 DAO 메소드에서 변치 않음
+			sql = "SELECT BOARD_NO, TITLE, CONTENT, WRITER, CREATED_AT, MODIFIED_AT FROM BOARD WHERE BOARD_NO = ?";
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, board_no);
+			rs = ps.executeQuery(); // 실행하는메소드
+			
+			if(rs.next()) { // 있으면
+				board = new BoardDTO(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6));
+				
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(); // 자원반납
+		}
+		return board;
+		
+	}
 	
+	// 삽입 수정 삭제 패턴이 같은거 이해하기.
 	// 3. 삽입
+	
+	public int insetBoard(BoardDTO board) {
+		int result = 0;
+		try {
+			con = getConnection(); // 시작은 커넥션 모든 DAO 메소드에서 변치 않음
+			sql = "INSERT INTO BOARD VALUES(BOARD_SEQ.NEXTVAL, ?, ?, ?, TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS'),TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS'))";
+			// 물음표3개니까
+			ps = con.prepareStatement(sql);
+			ps.setString(1, board.getTitle());
+			ps.setString(2, board.getContent());
+			ps.setString(3, board.getWriter());
+			result = ps.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(); // 자원반납
+		}
+		return result;
+	}
 	
 	
 	// 4. 수정
 	
+	public int updateBoard(BoardDTO board) {
+		int result = 0;
+		try {
+			con = getConnection(); // 시작은 커넥션 모든 DAO 메소드에서 변치 않음
+			sql = "UPDATE BOARD SET TITLE = ?, CONTENT = ?, MODIFIED_AT = TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS') WHERE BOARD_NO = ?";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, board.getTitle());
+			ps.setString(2, board.getContent());
+			ps.setInt(3, board.getBoard_no());
+			result = ps.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(); // 자원반납
+		}
+		return result;
+	}
+	
 	
 	// 5. 삭제
+	
+	public int deleteBoard(int board_no) {
+		int result = 0;
+		try {
+			con = getConnection(); // 시작은 커넥션 모든 DAO 메소드에서 변치 않음
+			sql ="DELETE FROM BOARD WHERE BOARD_NO = ?";
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, board_no);
+			result = ps.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(); // 자원반납
+		}
+		return result;
+	}
+
 
 
 }
---
+++ 04_Jdbc_JUnit/src/test/java/com/gdu/app04/BoardUnitTest.java
@@ -0,0 +1,123 @@
+package com.gdu.app04;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.gdu.app04.domain.BoardDTO;
+import com.gdu.app04.repository.BoardDAO;
+
+// JUnit4
+@RunWith(SpringJUnit4ClassRunner.class) // junit쓰겠다. (.class는 타입을 명시한다는 뜻)
+
+
+/*
+	 	@ContextConfiguration
+	 	1. 어떤 방식으로 Spring Container에 Bean을 등록했는지 알려주는 애너테이션이다. 역할이다.
+	 	2. Bean 등록 방식 (3가지)
+	 		1) root-context.xml에 <bean> 태그를 추가한 경우
+	 			@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+	 		
+	 		2) com.gdu.app04.config.AppConfig.java에 @Configuration + @Bean을 작성한 경우
+	 			@ContextConfiguration(classes={AppConfig.class})
+	 		
+	 		3) @Component(@Controller, @Service, @Repository)를 작성한 경우
+	 			@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+	 		
+	 		지금은 3)으로 하고 있음
+ */
+
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+
+/*
+ 	테스트 순서 결정하기
+ 	1. 순서 : 삽입 - 상세 - 수정 - 상세 - 목록 - 삭제
+ 	2. 테스트 메소드명을 기준으로 순서를 결정하는 애너테이션인 @FixMethodOrder를 사용한다.
+ 	
+ 	
+ */
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) // 메소드이름의 오름차순 알파벳순으로 테스트를 수행한다.
+public class BoardUnitTest {
+
+	// 지금까지 왜했냐. 단위테스트 때문에
+	// DAO 메소드 단위로 Unit 테스트를 진행하기 위해서 BoardDAO 타입의 Bean이 필요하다.
+	@Autowired
+	private BoardDAO boardDAO; // @ContextConfiguration 를 해야 @Autowired 가 된다.
+	
+	// 테스트 메소드
+	@Test
+	public void a1삽입테스트() {
+		BoardDTO board = new BoardDTO(0, "제목", "내용", "작성자", null, null);
+		assertEquals(1, boardDAO.insetBoard(board)); //boardDAO.insetBoard(board) 결과가 1이면 테스트 성공!
+		
+	}
+	
+	@Test
+	public void a2상세테스트() {
+		int board_no = 1; // 조회할 게시글의 번호
+		BoardDTO board = boardDAO.selectBoardByNo(board_no);
+		System.out.println("a2 - " + board); // LOG 기능으로 대체할 예정
+		assertNotNull(board); // board가 null이 아니면 테스트 성공!
+	}
+	
+	
+	@Test
+	public void a3수정테스트() {
+		BoardDTO board = new BoardDTO(1, "[수정]제목", "[수정]내용", null, null, null);
+		// 원리는 삽입이랑 똑같다.
+		assertEquals(1, boardDAO.updateBoard(board)); //boardDAO.updateBoard(board) 결과가 1이면 테스트 성공!
+		
+	}
+	
+	@Test
+	public void a4상세테스트() {
+		int board_no = 1; // 조회할 게시글의 번호
+		BoardDTO board = boardDAO.selectBoardByNo(board_no);
+		System.out.println("a4 - " + board); // LOG 기능으로 대체할 예정
+		assertNotNull(board); // board가 null이 아니면 테스트 성공!
+	}
+	
+	
+	@Test
+	public void a5목록테스트()	 {
+		List<BoardDTO> list = boardDAO.selectBoardList();
+		System.out.println("a5 - " + list); // LOG 기능으로 대체할 예정
+		assertEquals(1, list.size()); // 목록 개수가 1개이면 테스트 성공
+		
+	}
+	
+	@Test
+	public void a6삭제테스트() {
+		int board_no = 1;
+		assertEquals(1, boardDAO.deleteBoard(board_no));
+		
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
